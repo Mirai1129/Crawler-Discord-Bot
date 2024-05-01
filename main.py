99fbf6ff6d0ee7a21901403cef7dc5d609063312
YuChia Ma <79797958+Mirai1129@users.noreply.github.com>
2024-05-01 16:52:15 +0800

    :sparkles: add flask app, main

---
+++ main.py
@@ -0,0 +1,20 @@
+import threading
+import os
+
+
+def run_discord_bot():
+    os.system('cd Bot && python bot.py')
+
+
+def run_flask_app():
+    os.system('cd Route && python app.py')
+
+
+if __name__ == '__main__':
+    run_discord_bot_process = threading.Thread(target=run_discord_bot)
+    run_flask_app_process = threading.Thread(target=run_flask_app)
+    run_discord_bot_process.start()
+    run_flask_app_process.start()
+    run_discord_bot_process.join()
+    run_flask_app_process.join()
+    print('Bot process terminated.')
