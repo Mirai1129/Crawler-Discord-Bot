1bb81e31c19c1fa8e192f85c4da2c363ef5d29ae
YuChia Ma <minecraftmiku831@gmail.com>
2024-05-05 00:38:37 +0800

    :recycle: improve router improvement

--- Router/routes/database.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-database_blueprint = Blueprint('database', __name__)
-
-
-@database_blueprint.route('/data')
-def data():
-    return 'This is the data page of the database blueprint.'
