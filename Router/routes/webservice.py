6b1c76e9c7ac65a287f433eaf3100bc125f10863
YuChia Ma <minecraftmiku831@gmail.com>
2024-05-04 17:27:15 +0800

    rename folder

--- Router/routes/webservice.py
+++ Router/routes/webservice.py
@@ -1,9 +1,9 @@
 import os
 import flask
 
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../web', 'templates')
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../Web', 'templates')
 
-webservice_blueprint = flask.Blueprint('web', __name__, template_folder=template_dir)
+webservice_blueprint = flask.Blueprint('Web', __name__, template_folder=template_dir)
 
 
 @webservice_blueprint.route('/')
